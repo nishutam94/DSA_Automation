afb40956911988d38e6ef73ca1fa5a016559bcfd
paul luse <paul.e.luse@intel.com>
2021-06-17 16:36:23 +0530

    dsa-micros update

--- Sheet_transformer_micro.py
+++ Sheet_transformer_micro.py
@@ -7,10 +7,10 @@ parser.add_argument("-p", "--output_dir", help="Specify Path of logs",default=".
 parser.add_argument("-o", "--output", help="Specify name of summary",default="summary.csv")
 args = parser.parse_args()
 read_file = pd.read_csv (args.output_dir+'summary.csv')
-read_file.to_excel (args.output_dir+'summary.xlsx', index = None, header=True) 
+read_file.to_excel (args.output_dir+'summary.xlsx', index = None, header=False) 
 
 wb1 = xl.load_workbook(args.output_dir+'summary.xlsx') 
-print(wb1)
+#print(wb1)
 ws1 = wb1.worksheets[0] 
 wb = xl.Workbook()
 sheet_obj = wb.active
@@ -35,13 +35,13 @@ for op in list_of_op:
         row_count=row_count+1
         word = [] 
         word.append(queuedepth)
-        for i in range (2, mr + 1):
-            BW=(ws1.cell(row = i, column=6).value)
+        for i in range (1, mr + 1):
+            BW=(ws1.cell(row = i, column=9).value)
             #print(BW)
             op_1=(ws1.cell(row = i, column=1).value)
             qd_1=(ws1.cell(row = i, column=2).value)
             if (op_1==op and queuedepth==int(qd_1)):
-                word.append(ws1.cell(row = i, column=6).value)     
+                word.append(ws1.cell(row = i, column=9).value)     
             filldata(word,row_count)   
     row_count=row_count+1       
 wb.save(str(args.output_dir+"single_instance.xlsx"))
